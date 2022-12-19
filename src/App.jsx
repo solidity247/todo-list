95dd92c7975ceee8f4d2975553ab2283039dac50
Ermek Kanybekov <ekanybekov91@gmail.com>
2022-12-19 21:39:58 +0100

    ErmekCommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Todos from './components/todos'  
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Todos/>
     </div>
   )
 }
---
+++ src/components/Todos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Todos extends React.Component{
+    render(){
+        return(
+            <div>Hello</div>
+        )
+    }
+}
+
+export default Todos;
