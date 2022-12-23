e9a41bcab02178c6b892f22db42be9beec86a666
takhir <solidity247@gmail.com>
2022-12-23 00:20:59 -0500

    Builder updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+​
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  //-----GitHub Pages require these settings
+  base: "./",
+  build: {
+    outDir: "docs",
+  },
+  //-----GitHub Pages settings end
+});
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
